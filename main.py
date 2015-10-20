89f30834930edd8684d58d41b40de17cb2ad70bd
Jack Napolitano <jnapp18@gmail.com>
2015-10-20 15:07:02 -0400

    Added blank template and handler for Lobby and Challenges.
    Added blank template and handler for Lobby and Challenges.

--- main.py
+++ main.py
@@ -19,32 +19,42 @@ import webapp2
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import ndb
+################## NDB Models ####################
+#Account management table
+class accountModel(ndb.Model):
+  firstName = ndb.StringProperty()
+  lastName = ndb.StringProperty()
+  username = ndb.StringProperty()
+#Lobby management table
+class lobbyModel(ndb.Model):
+  lobbyID = ndb.IntegerProperty()
+  lobbyName = ndb.TextProperty()
+  publicBool = ndb.BooleanProperty()
+  ownerID = ndb.IntegerProperty()
+#Lobby Access table
+class lobbyAccessModel(ndb.Model):
+  lobbyID = ndb.IntegerProperty()
+  userID = ndb.IntegerProperty()
+#Challenge management table
+class challengeModel(ndb.Model):
+  challengeID = ndb.IntegerProperty()
+  ownerID = ndb.IntegerProperty()
+  question = ndb.TextProperty()
+  answer = ndb.TextProperty()
+  attachments = ndb.BlobProperty
+  score = ndb.IntegerProperty()
+#Challenge Access table
+class challengeAccessModel(ndb.Model):
+  challengeID = ndb.IntegerProperty()
+  lobbyID = ndb.IntegerProperty()
+#progress tracking table
+class progressTable(ndb.Model):
+  userID = ndb.IntegerProperty()
+  lobbyID = ndb.IntegerProperty()
+  challengeID = ndb.IntegerProperty()
+################## End NDB Models ####################
 
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-
-        email = get_user_email()
-        fname = ""
-        lname = ""
-        alias = ""
-        if email:
-          print 'fix'
-          qry = accountModel.get_by_id(users.get_current_user().user_id())
-          if qry:
-            fname = qry.firstName
-            lname = qry.lastName
-            alias = qry.alias
-        page_params = {
-          'user_email': email,
-          'firstName': fname,
-          'lastName': lname,
-          'alias': alias,
-          'login_url': users.create_login_url(),
-          'logout_url': users.create_logout_url('/')
-        }
-        render_template(self, 'index.html', page_params)
-
+################## Helper Functions ####################
 def render_template(handler, templatename, templatevalues={}):
   path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
   html = template.render(path, templatevalues)
@@ -56,39 +66,55 @@ def get_user_email():
   if user:
     result = user.email()
   return result
+################## End Helper Functions ####################
 
-
-###############################################################################
+################## Page Handlers ####################
+#Here we basically gather information, and send that information into a template so a page can be rendered.
+#Home page handler.
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        email = get_user_email()
+        fname = ""
+        lname = ""
+        username = ""
+        if email:
+          qry = accountModel.get_by_id(users.get_current_user().user_id())
+          if qry:
+            fname = qry.firstName
+            lname = qry.lastName
+            username = qry.username
+        page_params = {
+          'user_email': email,
+          'firstName': fname,
+          'lastName': lname,
+          'username': username,
+          'login_url': users.create_login_url(),
+          'logout_url': users.create_logout_url('/')
+        }
+        render_template(self, 'index.html', page_params)
+#"My Account" page handler for editing information
 class accountManageDisplay(webapp2.RequestHandler):
   def get(self):
     email = get_user_email()
     fname = ""
     lname = ""
-    alias = ""
+    username = ""
     if email:
-      print 'fix'
       qry = accountModel.get_by_id(users.get_current_user().user_id())
       if qry:
         fname = qry.firstName
         lname = qry.lastName
-        alias = qry.alias
+        username = qry.username
     page_params = {
       'user_email': email,
       'firstName': fname,
       'lastName': lname,
-      'alias': alias,
+      'username': username,
       'login_url': users.create_login_url(),
       'logout_url': users.create_logout_url('/')
     }
     render_template(self, 'acctManageInfo.html', page_params)
-
-
-class accountModel(ndb.Model):
-  firstName = ndb.StringProperty()
-  lastName = ndb.StringProperty()
-  alias = ndb.StringProperty()
-
-########################################################################################
+#Handler for "My Account" information. Displays information and updates ndb accountModel with new info.
 class accountManagementHandler(webapp2.RequestHandler):
   def get(self):
     email = get_user_email()
@@ -108,12 +134,12 @@ class accountManagementHandler(webapp2.RequestHandler):
     if email: 
       fname = self.request.get('fname')
       lname = self.request.get('lname')
-      alias = self.request.get('alias')
-
+      username = self.request.get('username')
+      #updating the database with account information
       AcctModel = accountModel(id=users.get_current_user().user_id())
       AcctModel.firstName = fname
       AcctModel.lastName = lname
-      AcctModel.alias = alias
+      AcctModel.username = username
       AcctModel.put()
       page_params = {
         'login_url': users.create_login_url(),
@@ -121,16 +147,67 @@ class accountManagementHandler(webapp2.RequestHandler):
         'user_email': email,
         'firstName': fname,
         'lastName': lname,
-        'alias': alias
+        'username': username
       }
       render_template(self, 'acctManageInfo.html', page_params)
     else:
       self.redirect('/')
+#Lobby Handler
+class LobbyHandler(webapp2.RequestHandler):
+    def get(self):
+        email = get_user_email()
+        fname = ""
+        lname = ""
+        username = ""
+        if email:
+          qry = accountModel.get_by_id(users.get_current_user().user_id())
+          if qry:
+            fname = qry.firstName
+            lname = qry.lastName
+            username = qry.username
+        page_params = {
+          'user_email': email,
+          'firstName': fname,
+          'lastName': lname,
+          'username': username,
+          'login_url': users.create_login_url(),
+          'logout_url': users.create_logout_url('/')
+        }
+        render_template(self, 'lobbies.html', page_params)
+class ChallengeHandler(webapp2.RequestHandler):
+    def get(self):
+        email = get_user_email()
+        fname = ""
+        lname = ""
+        username = ""
+        if email:
+          qry = accountModel.get_by_id(users.get_current_user().user_id())
+          if qry:
+            fname = qry.firstName
+            lname = qry.lastName
+            username = qry.username
+        page_params = {
+          'user_email': email,
+          'firstName': fname,
+          'lastName': lname,
+          'username': username,
+          'login_url': users.create_login_url(),
+          'logout_url': users.create_logout_url('/')
+        }
+        render_template(self, 'challenges.html', page_params)
+################## End Page Handlers ####################
 
-########################################################################################
+
+
+
+
+################## url Mappings. ####################
+#When a URL is clicked, goes to the function to take care of the specific request.
 mappings = [
   ('/', MainHandler),
   ('/index', MainHandler),
+  ('/Lobbies', LobbyHandler),
+  ('/Challenges', ChallengeHandler),
   ('/acctManage', accountManagementHandler),
   ('/acctManageInfo', accountManageDisplay)
 ]
